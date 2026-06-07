0e31605eb02d284988bc263eb7c9bc30e233d4d6
Michał Słonina <michal.slonina@gmail.com>
2026-06-07 15:31:10 +0200

    Add simple hello bot that prints hllo every 5 seconds

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bot1
+
+A simple bot example that prints "hllo" every 5 seconds.
+
+## Run the bot
+
+```bash
+node hello-bot.js
+```
+
---
+++ hello-bot.js
@@ -0,0 +1,7 @@
+// hello-bot.js
+// This bot says "hllo" every 5 seconds.
+// Run it with: node hello-bot.js
+
+setInterval(() => {
+  console.log('hllo');
+}, 5000);
